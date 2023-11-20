6e62be7bb265d23da45c05383cad96fb24ec71fa
Leo <deutschleonardo@gmail.com>
2023-11-20 10:29:57 -0600

    Created an endpoint to retrieve a single student by id using path variable.

--- src/main/java/com/leonardodeutsch/springrest/rest/StudentRestController.java
+++ src/main/java/com/leonardodeutsch/springrest/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.leonardodeutsch.springrest.rest;
 
 import com.leonardodeutsch.springrest.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +14,25 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    private List<Student> theStudents;
 
-        List<Student> theStudents = new ArrayList<>();
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Poornima", "Patel"));
         theStudents.add(new Student("Mario", "Rossi"));
         theStudents.add(new Student("Mary", "Smith"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
 
         return theStudents;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return theStudents.get(studentId);
     }
 }
